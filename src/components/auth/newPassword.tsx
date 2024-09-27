d0824bb670e706fcb097dae7d085cb5819058bfe
thibault <tjeanpierre757@gmail.com>
2024-09-27 09:29:48 +0200

    update favicon and newPassword

--- src/components/auth/newPassword.tsx
+++ src/components/auth/newPassword.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import CardWrapper from './cardWrapper'
 import Link from 'next/link'
+import InputString from './InputString'
+import ButtonForm from './buttonForm'
 
 const NewPassword = () => {
     return (
         <CardWrapper title='Changer votre mot de passe'>
-            new password page
-            <Link
-                href={'/auth/login'}
-                className='flex items-center justify-center text-sm text-gray-500 hover:text-gray-700'
-            >
-                Se connecter
-            </Link>
+            <div className='px-8 py-4 space-y-4'>
+                <InputString
+                    title='Email'
+                    placeholder='John@doe.com'
+                    type='email'
+                />
+            </div>
+            <div className='flex justify-center items-center mt-4 w-full px-8'>
+                <ButtonForm
+                    title="Reinitialiser mon mot de passe"
+                />
+            </div>
+            <div className='mt-10'>
+                <Link
+                    href={'/auth/login'}
+                    className='flex items-center justify-center text-sm text-gray-500 hover:text-gray-700'
+                >
+                    Se connecter
+                </Link>
+            </div>
         </CardWrapper>
     )
 }
