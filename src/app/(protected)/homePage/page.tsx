f4cffa4ab7590c47705eb51644cfdde960b99e0c
jpthibault20 <tjeanpierre757@gmail.com>
2024-09-30 20:34:09 +0200

    add requireAuth component for auth

--- src/app/(protected)/homePage/page.tsx
+++ src/app/(protected)/homePage/page.tsx
@@ -1,44 +1,41 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
-import { createClient } from '@/utils/supabase/server'
-import { redirect } from 'next/navigation'
 import { signOut } from '@/app/auth/login/action'
+import RequireAuth from '@/components/auth/requireAuth'
+import { createClient } from '@/utils/supabase/server'
 
 
 const page = async () => {
     const supabase = createClient()
-
     const {
         data: { user },
     } = await supabase.auth.getUser();
 
-    if (!user) {
-        return redirect("/auth/login");
-    }
-
     return (
-        <div>
-            <form action={signOut}>
-                <p className='text-2xl font-istok'>homePage</p>
-                <p>
-                    id : {user.id}
-                    <br />
-                    email : {user.email}
-                    <br />
-                    phone : {user.phone}
-                    <br />
-                    created_at : {user.created_at}
-                    <br />
-                    role : {user.role}
-                    <br />
-                    email confimed : {user.email_confirmed_at}
-                    <br />
-                    aud : {user.aud}
+        <RequireAuth redirectToAuth={true}>
+            <div>
+                <form action={signOut}>
+                    <p className='text-2xl font-istok'>homePage</p>
+                    <p>
+                        id : {user?.id}
+                        <br />
+                        email : {user?.email}
+                        <br />
+                        phone : {user?.phone}
+                        <br />
+                        created_at : {user?.created_at}
+                        <br />
+                        role : {user?.role}
+                        <br />
+                        email confimed : {user?.email_confirmed_at}
+                        <br />
+                        aud : {user?.aud}
 
-                </p>
-                <Button>Sign out</Button>
-            </form>
-        </div>
+                    </p>
+                    <Button>Sign out</Button>
+                </form>
+            </div>
+        </RequireAuth>
     )
 }
 
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,25 +1,18 @@
-import { createClient } from '@/utils/supabase/server'
 import { Login } from '@/components/auth/login'
+import RequireAuth from '@/components/auth/requireAuth'
 import React, { Suspense } from 'react'
-import { redirect } from 'next/navigation'
 
 const LoginPage = async () => {
-    const supabase = createClient()
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
 
-    if (user) {
-        return redirect("/homePage");
-    }
-    
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div className="">
-                <Login />
-            </div>
-        </Suspense>
+        <RequireAuth redirectToAuth={false}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <div className="">
+                    <Login />
+                </div>
+            </Suspense>
+        </RequireAuth>
     )
 }
 
