aedb1c9c42fca43eafe9189d957ab71415026ad1
thibault <tjeanpierre757@gmail.com>
2024-10-07 15:39:38 +0200

    add layout to protected pages

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,12 @@
+
+export default function ProtectLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
