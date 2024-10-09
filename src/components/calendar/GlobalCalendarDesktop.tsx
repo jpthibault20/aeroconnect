c1cddef798a77faa40279a1cc241fc8bcfcdbb48
jpthibault20 <tjeanpierre757@gmail.com>
2024-10-09 16:33:49 +0200

    vercel

--- src/components/calendar/GlobalCalendarDesktop.tsx
+++ src/components/calendar/GlobalCalendarDesktop.tsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react'
 import InitialLoading from '../InitialLoading'
-// import { getDate } from '@/api/date'
+import { getDate } from '@/api/date'
 
 const GlobalCalendarDesktop = () => {
     useEffect(() => {
         const date =  new Date();
-        console.log(date)
-        // const fetchDate = async () => {
-        //     try {
-        //         const res = await getDate();
-        //         console.log(res)
-        //     } catch (error) {
-        //         console.log(error)
-        //     }
-        // }
-        // fetchDate()
+        console.log("client date : ", date)
+
+        const fetchDate = async () => {
+            try {
+                const res = await getDate();
+                console.log("server date : ", res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchDate()
 
     }, [])
     return (
