0c8a77d6327a13a29eb0996e62e49e17d5b09692
jpthibault20 <tjeanpierre757@gmail.com>
2024-10-09 16:48:43 +0200

    vercel

--- src/api/date.ts
+++ src/api/date.ts
@@ -2,19 +2,10 @@
 
 export const getDate = async () => {
     const currentDate = new Date();
+    
 
-    const formattedDate = new Intl.DateTimeFormat('fr-FR', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZoneName: 'short',
-      }).format(currentDate);
-
-
-    return {formattedDate
+    return {
+        currentDate
     }
 
 }
--- src/components/calendar/GlobalCalendarDesktop.tsx
+++ src/components/calendar/GlobalCalendarDesktop.tsx
@@ -4,13 +4,23 @@ import { getDate } from '@/api/date'
 
 const GlobalCalendarDesktop = () => {
     useEffect(() => {
-        const date =  new Date();
+        const date = new Date();
         console.log("client date : ", date)
 
         const fetchDate = async () => {
             try {
                 const res = await getDate();
-                console.log("server date : ", res)
+                const formattedDate = new Intl.DateTimeFormat('fr-FR', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    timeZoneName: 'short',
+                }).format(res.currentDate);
+                
+                console.log("server date : ", formattedDate)
             } catch (error) {
                 console.log(error)
             }
