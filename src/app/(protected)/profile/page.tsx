fb33765eb60bf59940b0c81255664b414f2de392
jpthibault20 <tjeanpierre757@gmail.com>
2024-10-09 10:08:06 +0200

    button signout for phane view

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,9 +1,19 @@
+"use client"
+import { signOut } from '@/app/auth/login/action'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
     return (
         <div>
             profile page
+            <div className='lg:hidden'>
+                <Button 
+                className=''
+                    onClick={() => signOut()}>
+                    Déconnexion
+                </Button>
+            </div>
         </div>
     )
 }
