31bb0086abe7d19d25844a77efd2e8c39fbd6c7f
jpthibault20 <tjeanpierre757@gmail.com>
2024-12-04 19:35:41 +0100

    update bg

--- src/components/dashboard/PageComponent.tsx
+++ src/components/dashboard/PageComponent.tsx
@@ -17,7 +17,7 @@ interface PageProps {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const PageComponent = ({ clubID, HoursByMonth, HoursByInstructor, HoursByPlane }: PageProps) => {
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-gray-200">
             <Header clubName="Aeroculb ULM du saulnois" />
             <main className="container mx-auto px-4 py-7">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
