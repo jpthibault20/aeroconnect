b97c58104bccb21b614d3c0ea5510f6aefd9a601
thibault <tjeanpierre757@gmail.com>
2024-12-12 14:53:43 +0100

    deashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 "use server"
-// import { getAllUserRequestedClubID } from '@/api/db/club';
-// import { getHoursByInstructor, getHoursByMonth, getHoursByPlane, getHoursByStudent } from '@/api/db/sessions';
+import { getAllUserRequestedClubID } from '@/api/db/club';
+import { getHoursByInstructor, getHoursByMonth, getHoursByPlane, getHoursByStudent } from '@/api/db/sessions';
 import PageComponent from '@/components/dashboard/PageComponent';
 import InitialLoading from '@/components/InitialLoading';
 import NoClubID from '@/components/NoClubID';
@@ -19,32 +19,32 @@ const Page = async ({ searchParams }: PageProps) => {
 
     if (clubID) {
 
-        // const [hoursByPlanes, HoursByInstructor, UsersRequestedClubID, HoursByMonth, HoursByStudent] = await Promise.all([
-        //     await getHoursByPlane(clubID),
-        //     await getHoursByInstructor(clubID),
-        //     await getAllUserRequestedClubID(clubID),
-        //     await getHoursByMonth(clubID),
-        //     await getHoursByStudent(clubID),
-        // ]);
+        const [hoursByPlanes, HoursByInstructor, UsersRequestedClubID, HoursByMonth, HoursByStudent] = await Promise.all([
+            await getHoursByPlane(clubID),
+            await getHoursByInstructor(clubID),
+            await getAllUserRequestedClubID(clubID),
+            await getHoursByMonth(clubID),
+            await getHoursByStudent(clubID),
+        ]);
 
-        // if ('error' in UsersRequestedClubID) {
-        //     console.error(UsersRequestedClubID.error);
-        //     return (
-        //         <div className="h-full">
-        //             {UsersRequestedClubID.error}
-        //         </div>
-        //     );
-        // }
+        if ('error' in UsersRequestedClubID) {
+            console.error(UsersRequestedClubID.error);
+            return (
+                <div className="h-full">
+                    {UsersRequestedClubID.error}
+                </div>
+            );
+        }
 
         return (
             <InitialLoading clubIDURL={clubID} className="h-full w-full">
                 <PageComponent
                     clubID={clubID}
-                    HoursByInstructor={[]}
-                    UsersRequestedClubID={[]}
-                    HoursByMonth={[]}
-                    HoursByStudent={[]}
-                    hoursByPlanes={[]} />
+                    HoursByInstructor={HoursByInstructor}
+                    UsersRequestedClubID={UsersRequestedClubID}
+                    HoursByMonth={HoursByMonth}
+                    HoursByStudent={HoursByStudent}
+                    hoursByPlanes={hoursByPlanes} />
             </InitialLoading>
         )
     }
