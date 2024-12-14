f64243db7a02c2486b6e22d1d138b1e31e236912
jpthibault20 <tjeanpierre757@gmail.com>
2024-12-14 13:49:36 +0100

    test prod

--- src/api/popupCalendar.ts
+++ src/api/popupCalendar.ts
@@ -72,7 +72,7 @@ export const getFreePlanesUsers = async (
 
     // Filtrer les utilisateurs avec le rôle "admin" et les étudiants disponibles
     const students = usersProp.filter(
-        user => user.role === userRole.ADMIN && !usedStudentIDs.includes(user.id)
+        user => user.role !== userRole.ADMIN && !usedStudentIDs.includes(user.id)
     );
 
     // Filtrer les avions disponibles
