862f271ba081b70c4c1ff97dd5200aea85f517fe
thibault <tjeanpierre757@gmail.com>
2024-12-23 10:22:46 +0100

    tast prod login more speed

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,25 +1,16 @@
 "use server"
-import { getUser } from '@/api/db/users'
 import { Login } from '@/components/auth/login'
-import { redirect } from 'next/navigation';
 import React, { Suspense } from 'react'
 
 const LoginPage = async () => {
-    const user = await getUser();
 
-    if (user.error) {
-        return (
-            <Suspense fallback={<div>Loading...</div>}>
-                <div className="">
-                    <Login />
-                </div>
-            </Suspense>
-        )
-    }
-    else {
-        redirect(`/calendar?clubID=${user.user?.clubID || ''}`)
-    }
-
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="">
+                <Login />
+            </div>
+        </Suspense>
+    )
 
 }
 
