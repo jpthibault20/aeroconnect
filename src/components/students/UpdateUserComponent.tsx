def2751552350cbfe64668d7fbdf53d95b149c92
thibault <tjeanpierre757@gmail.com>
2025-01-02 16:10:13 +0100

    add update all user

--- src/components/students/UpdateUserComponent.tsx
+++ src/components/students/UpdateUserComponent.tsx
@@ -12,6 +12,8 @@ import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 import { useCurrentUser } from '@/app/context/useCurrentUser'
 import { Spinner } from '../ui/SpinnerVariants'
+import InputClasses from '../InputClasses'
+import { ScrollArea } from '../ui/scroll-area'
 
 
 
@@ -27,22 +29,8 @@ const UpdateUserComponent = ({ children, showPopup, setShowPopup, setUsers, user
     const { currentUser } = useCurrentUser();
     const [loading, setLoading] = useState(false);
     const [autorisedModifyRole, setAutorisedModifyRole] = useState(false);
-    const [userState, setUserState] = useState<User>({
-        id: user.id || '',
-        firstName: user.firstName || '',
-        lastName: user.lastName || '',
-        email: user.email || '',
-        phone: user.phone || null,
-        adress: user.adress || null,
-        city: user.city || null,
-        zipCode: user.zipCode || null,
-        role: user.role || userRole.USER,
-        clubID: user.clubID || '',
-        restricted: user.restricted || false,
-        country: user.country || null,
-        clubIDRequest: user.clubIDRequest || null,
-        classes: user.classes || [1, 2, 3, 4, 5, 6],
-    })
+    const [classes, setClasses] = useState<number[]>(user.classes);
+    const [userState, setUserState] = useState<User>(user);
 
     useEffect(() => {
         if (currentUser?.role === "ADMIN" || currentUser?.role === "OWNER" || currentUser?.role === "INSTRUCTOR") {
@@ -51,6 +39,11 @@ const UpdateUserComponent = ({ children, showPopup, setShowPopup, setUsers, user
             setAutorisedModifyRole(false);
         }
     }, [currentUser]);
+
+    useEffect(() => {
+        setUserState({ ...userState, classes })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [classes])
 
     const onChangeUserState = (key: keyof typeof userState, value: string | boolean) => {
         setUserState((prev) => ({
@@ -116,137 +109,147 @@ const UpdateUserComponent = ({ children, showPopup, setShowPopup, setUsers, user
                         Modifiez les informations du profil de l&apos;utilisateur
                     </DialogDescription>
                 </DialogHeader>
-                <div className='space-y-3 max-h-[60vh] md:max-h-none overflow-y-auto md:overflow-visible'>
-                    {/* Nom et Prénom */}
-                    <div className="grid grid-cols-2 gap-4 py-2">
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="firstName">Nom</Label>
-                            <Input
-                                id="firstName"
-                                value={userState.firstName}
-                                disabled={loading}
-                                onChange={(e) => onChangeUserState('firstName', e.target.value)}
-                            />
-                        </div>
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="lastName">Prénom</Label>
-                            <Input
-                                id="lastName"
-                                value={userState.lastName || ''}
-                                disabled={loading}
-                                onChange={(e) => onChangeUserState('lastName', e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    {/* Email */}
-                    <div className="grid items-center gap-2">
-                        <Label htmlFor="email">Adresse email</Label>
-                        <Input
-                            id="email"
-                            value={userState.email}
-                            disabled={true}
-                            onChange={(e) => onChangeUserState('email', e.target.value)}
-                        />
-                    </div>
-
-                    {/* Téléphone */}
-                    <div className="grid items-center gap-2">
-                        <Label htmlFor="phone">Téléphone</Label>
-                        <Input
-                            id="phone"
-                            value={userState.phone || ''}
-                            disabled={loading}
-                            onChange={(e) => onChangeUserState('phone', e.target.value)}
-                        />
-                    </div>
-
-                    {/* Adresse */}
-                    <div className="grid items-center gap-2">
-                        <Label htmlFor="adress">Adresse</Label>
-                        <Input
-                            id="adress"
-                            value={userState.adress || ''}
-                            disabled={loading}
-                            onChange={(e) => onChangeUserState('adress', e.target.value)}
-                        />
-                    </div>
-
-                    {/* Ville et Code Postal */}
-                    <div className="grid grid-cols-2 gap-4 py-2">
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="city">Ville</Label>
-                            <Input
-                                id="city"
-                                value={userState.city || ''}
-                                disabled={loading}
-                                onChange={(e) => onChangeUserState('city', e.target.value)}
-                            />
-                        </div>
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="zipcode">Code postal</Label>
-                            <Input
-                                id="zipcode"
-                                value={userState.zipCode || ''}
-                                disabled={loading}
-                                onChange={(e) => onChangeUserState('zipCode', e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    {/* Rôle et clubID */}
-                    <div className="grid grid-cols-2 gap-4 py-2">
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="role">Rôle</Label>
-                            <Select
-                                value={userState.role}
-                                disabled={loading}
-                                onValueChange={(val: userRole) => onChangeUserState('role', val)}
-                            >
-                                <SelectTrigger className="w-[150px]" disabled={!autorisedModifyRole}>
-                                    <SelectValue placeholder="Rôle" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {Object.entries(userRole)
-                                        .filter(([key]) => key !== "ADMIN") // Exclure "ADMIN"
-                                        .map(([key, value]) => (
-                                            <SelectItem key={key} value={value}>
-                                                {key === "USER" && "Visiteur"}
-                                                {key === "STUDENT" && "Elève"}
-                                                {key === "INSTRUCTOR" && "Instructeur"}
-                                                {key === "PILOT" && "Pilote"}
-                                                {key === "OWNER" && "Président"}
-                                            </SelectItem>
-                                        ))}
-                                </SelectContent>
-                            </Select>
-                        </div>
-                        <div className="grid items-center gap-2">
-                            <Label htmlFor="clubID">Identifiant club</Label>
-                            <Input
-                                id="clubID"
-                                value={userState.clubID as string}
-                                disabled
-                            />
-                        </div>
-                    </div>
-
-                    {/* Restricted*/}
-                    <div className="grid items-center gap-2">
-                        <Label htmlFor="restricted">Restreindre l&apos;utilisateur</Label>
-                        <div className='flex items-center gap-2 border border-gray-300 rounded-xl p-2'>
-                            <p className='text-gray-500'>Un utilisateur restreint ne peut que visualiser les sessions </p>
-                            <div className='flex items-center gap-2'>
-                                <ImCross color='red' />
-                                <Switch
-                                    checked={userState.restricted}
-                                    onCheckedChange={(checked) => onChangeUserState('restricted', checked)}
+                <ScrollArea className='h-[500px]' >
+                    <div className='space-y-3 max-h-[60vh] md:max-h-none overflow-y-auto md:overflow-visible'>
+                        {/* Nom et Prénom */}
+                        <div className="grid grid-cols-2 gap-4 py-2">
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="firstName">Nom</Label>
+                                <Input
+                                    id="firstName"
+                                    value={userState.firstName}
+                                    disabled={loading}
+                                    onChange={(e) => onChangeUserState('firstName', e.target.value)}
                                 />
-                                <FaCheck color='green' />
+                            </div>
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="lastName">Prénom</Label>
+                                <Input
+                                    id="lastName"
+                                    value={userState.lastName || ''}
+                                    disabled={loading}
+                                    onChange={(e) => onChangeUserState('lastName', e.target.value)}
+                                />
+                            </div>
+                        </div>
+
+                        {/* Email */}
+                        <div className="grid items-center gap-2">
+                            <Label htmlFor="email">Adresse email</Label>
+                            <Input
+                                id="email"
+                                value={userState.email}
+                                disabled={true}
+                                onChange={(e) => onChangeUserState('email', e.target.value)}
+                            />
+                        </div>
+
+                        {/* Téléphone */}
+                        <div className="grid items-center gap-2">
+                            <Label htmlFor="phone">Téléphone</Label>
+                            <Input
+                                id="phone"
+                                value={userState.phone || ''}
+                                disabled={loading}
+                                onChange={(e) => onChangeUserState('phone', e.target.value)}
+                            />
+                        </div>
+
+                        {/* Adresse */}
+                        <div className="grid items-center gap-2">
+                            <Label htmlFor="adress">Adresse</Label>
+                            <Input
+                                id="adress"
+                                value={userState.adress || ''}
+                                disabled={loading}
+                                onChange={(e) => onChangeUserState('adress', e.target.value)}
+                            />
+                        </div>
+
+                        {/* Ville et Code Postal */}
+                        <div className="grid grid-cols-2 gap-4 py-2">
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="city">Ville</Label>
+                                <Input
+                                    id="city"
+                                    value={userState.city || ''}
+                                    disabled={loading}
+                                    onChange={(e) => onChangeUserState('city', e.target.value)}
+                                />
+                            </div>
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="zipcode">Code postal</Label>
+                                <Input
+                                    id="zipcode"
+                                    value={userState.zipCode || ''}
+                                    disabled={loading}
+                                    onChange={(e) => onChangeUserState('zipCode', e.target.value)}
+                                />
+                            </div>
+                        </div>
+
+                        {/* Rôle et clubID */}
+                        <div className="grid grid-cols-2 gap-4 py-2">
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="role">Rôle</Label>
+                                <Select
+                                    value={userState.role}
+                                    disabled={loading}
+                                    onValueChange={(val: userRole) => onChangeUserState('role', val)}
+                                >
+                                    <SelectTrigger className="w-[150px]" disabled={!autorisedModifyRole}>
+                                        <SelectValue placeholder="Rôle" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {Object.entries(userRole)
+                                            .filter(([key]) => key !== "ADMIN") // Exclure "ADMIN"
+                                            .map(([key, value]) => (
+                                                <SelectItem key={key} value={value}>
+                                                    {key === "USER" && "Visiteur"}
+                                                    {key === "STUDENT" && "Elève"}
+                                                    {key === "INSTRUCTOR" && "Instructeur"}
+                                                    {key === "PILOT" && "Pilote"}
+                                                    {key === "OWNER" && "Président"}
+                                                </SelectItem>
+                                            ))}
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div className="grid items-center gap-2">
+                                <Label htmlFor="clubID">Identifiant club</Label>
+                                <Input
+                                    id="clubID"
+                                    value={userState.clubID as string}
+                                    disabled
+                                />
+                            </div>
+                        </div>
+
+                        <div className="grid grid-cols-1 gap-4 py-2">
+                            <InputClasses
+                                disabled={loading}
+                                classes={classes}
+                                setClasses={setClasses}
+                            />
+                        </div>
+
+                        {/* Restricted*/}
+                        <div className="grid items-center gap-2">
+                            <Label htmlFor="restricted">Restreindre l&apos;utilisateur</Label>
+                            <div className='flex items-center gap-2 border border-gray-300 rounded-xl p-2'>
+                                <p className='text-gray-500'>Un utilisateur restreint ne peut que visualiser les sessions </p>
+                                <div className='flex items-center gap-2'>
+                                    <ImCross color='red' />
+                                    <Switch
+                                        checked={userState.restricted}
+                                        onCheckedChange={(checked) => onChangeUserState('restricted', checked)}
+                                    />
+                                    <FaCheck color='green' />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </ScrollArea >
                 <DialogFooter>
                     <button onClick={() => setShowPopup(false)} disabled={loading}>Annuler</button>
                     <Button onClick={onClickUpdateUser} disabled={loading}>
