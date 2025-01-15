4737ad3cdb469164a08c7609dcf1d43d94fca24b
thibault <tjeanpierre757@gmail.com>
2025-01-15 16:31:23 +0100

    uodate

---
+++ src/components/dashboard/AcceptMemberInClub.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalBody,
+    ModalFooter,
+    Button,
+    useDisclosure,
+} from "@nextui-org/react";
+
+const AcceptMemberInClub = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AcceptMemberInClub
