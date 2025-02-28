2366fcfb0cd51c1dbc2da9773c500732ff34d51d
jpthibault20 <tjeanpierre757@gmail.com>
2025-02-28 19:26:04 +0100

    Create new component "InvitedForm" available from add student with "invité" for user selection

--- src/components/flights/InvitedForm.tsx
+++ src/components/flights/InvitedForm.tsx
@@ -1,5 +1,7 @@
 import { InvitedStudent } from '@/api/db/users'
 import React from 'react'
+import { Label } from '../ui/label'
+import { Input } from '../ui/input'
 
 interface Props {
     invitedStudent: InvitedStudent
@@ -7,11 +9,56 @@ interface Props {
 }
 
 const InvitedForm = ({ invitedStudent, setInvitedStudent }: Props) => {
-  return (
-    <div>
-      InvitedForm
-    </div>
-  )
+    return (
+        <div className='space-y-3'>
+            <div className='flex items-center justify-between'>
+                <div
+                    className="items-center"
+                >
+                    <Label>Nom</Label>
+                    <Input
+                        type="text"
+                        className='w-full'
+                        value={invitedStudent.lastName}
+                        onChange={(e) => setInvitedStudent((prev) => ({ ...prev, lastName: e.target.value }))}
+                    />
+                </div>
+                <div
+                    className="items-center"
+                >
+                    <Label>Prénom</Label>
+                    <Input
+                        type="text"
+                        className='w-full'
+                        value={invitedStudent.firstName}
+                        onChange={(e) => setInvitedStudent((prev) => ({ ...prev, firstName: e.target.value }))}
+                    />
+                </div>
+            </div>
+            <div
+                className="flex items-center space-x-2"
+            >
+                <Label>Email</Label>
+                <Input
+                    type="text"
+                    className='w-full'
+                    value={invitedStudent.email}
+                    onChange={(e) => setInvitedStudent((prev) => ({ ...prev, email: e.target.value }))}
+                />
+            </div>
+            <div
+                className="flex items-center space-x-2"
+            >
+                <Label>Téléphone</Label>
+                <Input
+                    type="text"
+                    className='w-full'
+                    value={invitedStudent.phone}
+                    onChange={(e) => setInvitedStudent((prev) => ({ ...prev, phone: e.target.value }))}
+                />
+            </div>
+        </div>
+    )
 }
 
 export default InvitedForm
