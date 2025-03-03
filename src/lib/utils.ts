2a6fbe0eb60d244923bbe6f15a3321781bbee8f6
jpthibault20 <tjeanpierre757@gmail.com>
2025-03-03 23:21:43 +0100

    [FEAT : utils]
    add global function for email section (can mov for global project later)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
+import { Club } from "@prisma/client";
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export enum receiveType {
+  pilote,
+  student,
+  all,
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formattedDate = (date: Date) => {
+  const formatedDateString = date.toISOString();
+
+  return (`${formatedDateString.slice(8, 10)}/${formatedDateString.slice(5, 7)}/${formatedDateString.slice(0, 4)} ${formatedDateString.slice(11, 19)}`)
+
+}
+
+export const formatClubAdressString = (club: Club) => {
+  return `${club.Country} ${club.ZipCode} ${club.City} ${club.Address}`
 }
