8fed19fed0ee9bea905cd44a435a43c288ad3db8
thibault <tjeanpierre757@gmail.com>
2026-05-02 18:51:32 +0200

    prepar push to master

--- src/api/prisma.ts
+++ src/api/prisma.ts
@@ -1,17 +1,14 @@
 // src/api/prisma.ts
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-// Déclarez le type étendu
 const prismaClientSingleton = () => {
   return new PrismaClient({
-    log: process.env.NODE_ENV === 'development' 
-      ? ['query', 'error', 'warn'] 
+    log: process.env.NODE_ENV === 'development'
+      ? ['query', 'error', 'warn']
       : ['error'],
-  }).$extends(withAccelerate())
+  })
 }
 
-// Utilisez ReturnType pour capturer le type exact
 declare global {
   var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
 }
